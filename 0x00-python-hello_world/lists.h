bba63cb73b3267c03e10ac37c50a4816abf1b4f5
Rewantaha <rewantaha5p@gmail.com>
2023-11-28 16:54:42 -0800

    python

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,14 +4,14 @@
 #include <stdlib.h>
 
 
-typedef struct listin_s
+typedef struct listint_s
 {
 	int n;
-	struct listin_s *next;
-} listin_t;
+	struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
-listin_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
